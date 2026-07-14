250fe9b754e19287b6681fda4e61db922a80f77e
Qoder-Voidd <aliyushamoo93@gmail.com>
2026-07-14 19:39:41 +0100

    update learning section
    update learning section

--- Frontend/app/components/LearningSection.tsx
+++ Frontend/app/components/LearningSection.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function LearningSection() {
   return (
+    //article
     <article className="flex flex-col justify-center w-full items-center px-6">
+      //section
       <section className="pl-6 bg-[url(/images/learningSection.jpg)] bg-cover max-w-7xl rounded-t-4xl pt-30 pb-20 relative before:absolute before:w-full before:h-[30%] before:bottom-0 before:left-0 before:bg-linear-to-b before:from-transparent before:to-[#18151C] before:z-2 sm:flex sm:items-center sm:justify-between w-full sm:gap-5">
         <div className="mb-8 sm:mb-0">
           <h2 className="font-serif text-3xl sm:text-4xl lg:text-[3.25rem] lg:leading-[1.15] font-light leading-tight tracking-tight text-white my-5">
